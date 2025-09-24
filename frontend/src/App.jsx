2cc2b8e3ea13d70e711d9f100f18ba70519535fe
04Shubhampatil <04Shubhampatil>
2025-09-24 20:22:49 +0530

    frontend
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Button from "@mui/material/Button";
+
+function App() {
+
+  return (
+    < >
+     <center className='text-3xl bg-amber-500 font-bold'>
+      hello by jsx
+     </center>
+     <Button variant="contained" color='primary'>Hello</Button>
+    </>
+  )
+}
+
+export default App
